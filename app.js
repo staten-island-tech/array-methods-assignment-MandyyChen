ad77e412105b00a8714bec3c76aa0aa9be36ed7d
MandyyChen <mandychen022@gmail.com>
2023-10-10 10:11:25 -0400

    10/10

--- app.js
+++ app.js
@@ -2,23 +2,36 @@ const students = [
 { 
 firstName: "Sally",
 lastName: "Peach",
+nums: [2,4,6,3],
+female: true,
 },
+
 {
 firstName: "Ronald",
 lastName: "Apple",
+nums: [1,10,22,8],
+female: false,
 },
+
 {
 firstName: "Jack",
 lastName: "Orange",
+nums: [5,7,9,11],
+female: true,
 },
+
 {
 firstName: "Karen",
-lastName: "Kiwi"
+lastName: "Kiwi",
+nums: [13,15,16,17],
+female: false,
 }
 ];
 
 
 students.forEach(element => console.log(element.firstName));
+students.forEach(element => console.log(nums[nums.length - 1]));
+
 //arrays store items and can access items by their index starting at 0
 /*const nums = [1, 2, 3, 4, 5];
 console.log(nums[nums.length - 1]);
@@ -26,9 +39,9 @@ console.log(nums[nums.length - 1]);
 //foreach to run function on every item (for loop)
 nums.forEach((el)=> console.log(el))*/
 //arrow functions ()=> are simpler, auto return its output, this keyword changes
-const graduates = students.filter((student) => student.graduated);
-console.log(graduates);
+const female = students.filter((student) => student.female);
+console.log(female);
 
 // can use !== true or === false to demonstrate is something is false
-const current = students.filter((student) => student.graduated !== true);
-console.log(current);
+/* const current = students.filter((student) => student.graduated !== true);
+console.log(current); */
